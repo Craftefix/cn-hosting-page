7ac60d87c183d7440550c0c09a4a46be8df44e7f
Craftefix <github@craftefix.dev>
2025-05-29 13:08:57 +0200

    pls work

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
 import { Toaster } from "@/components/ui/toaster";
+
+
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { useQueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import CheckoutPage from "./pages/Checkout";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new useQueryClient();
+const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
