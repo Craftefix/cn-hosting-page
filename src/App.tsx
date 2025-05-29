e8cfbc598efacc4ff0f4aac03b46b0529e210050
Craftefix <github@craftefix.dev>
2025-05-29 12:59:41 +0200

    im dumb ig

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { useQueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+const queryClient = new useQueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
